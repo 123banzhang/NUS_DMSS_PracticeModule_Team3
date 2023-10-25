6488a06c6bb963497ddb5f1a2daf68b85abf14f9
Qitu <969977237@qq.com>
2023-10-25 20:49:57 +0800

    [feat] Init Micro Auth connector

--- src/main/java/com/sys/utils/interceptor/AuthInterceptor.java
+++ src/main/java/com/sys/utils/interceptor/AuthInterceptor.java
@@ -2,24 +2,33 @@ package com.sys.utils.interceptor;
 
 import com.sys.vo.RespBean;
 import com.sys.vo.RespBeanEnum;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.sys.authApi;
+import com.sys.vo.authVo;
+import com.sys.entity.User;
 
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private authApi authApi;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object user = request.getSession().getAttribute("user");
-
-        if (user == null) {
+        authVo authReq = new authVo();
+        authReq.token = request.getHeader("AuthToken");
+        User userInfo = authApi.verifyUser(authReq);
+        if (userInfo == null) {
             response.setContentType("application/json");
             response.getWriter().write(RespBean.error(RespBeanEnum.LOGIN_ERROR).toString());
             return false;
         }
+        request.setAttribute("userInfo", userInfo);
         return true;
     }
 }
