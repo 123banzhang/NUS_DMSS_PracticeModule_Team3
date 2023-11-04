851d375da49b364fed2c05b9243256cb4957cb01
丨FN丨 <8282167+tylorzeng@user.noreply.gitee.com>
2023-11-04 16:50:51 +0800

    add switch model feature in gptApiController

--- src/main/java/com/client/controller/OpenAiApiController.java
+++ src/main/java/com/client/controller/OpenAiApiController.java
@@ -2,6 +2,9 @@ package com.client.controller;
 
 import com.client.entitiy.Message;
 import com.client.entitiy.ChatRequest;
+import com.client.service.AnotherLanguageModelService;
+import com.client.service.LanguageModelService;
+import com.client.service.OpenAiLanguageModelService;
 import com.sys.service.impl.MetahumanServiceImpl;
 import com.sys.vo.MetahumanDetailVo;
 import org.json.JSONObject;
@@ -32,17 +35,20 @@ public class OpenAiApiController {
     @Autowired
     private MetahumanServiceImpl metahumanService;  // 注入MetahumanServiceImpl
 
-    //    private String generatePromptFromMetahumanDetail(MetahumanDetailVo detail) {
-//        // 基于你的需求和Metahuman的属性，构造合适的提示
-//        return String.format("你正在与%s对话，他/她是%s，简介为：%s。",
-//                detail.getName(), detail.getCategory(), detail.getDescription());
-//    }
     // 将MetaHuman的详细信息转换为提示
     private String convertMetahumanToPrompt(MetahumanDetailVo metahuman) {
         // 基于你的需求和Metahuman的属性，构造合适的提示
-        return String.format("Currently, you are addressed as %s, identified by the gender %s. Below are the specific tasks you are expected to perform, the knowledge base you should rely on, or the guidelines on how users anticipate your responses: %s.",
-                metahuman.getName(), metahuman.getGender(), metahuman.getDescription());
+        return String.format("Currently, your name is set as %s, identified by the gender %s. Below are the specific tasks you are expected to perform, the knowledge base you should rely on, or the guidelines on how users anticipate your responses: %s. This profile was created on %s and last updated on %s.",
+                metahuman.getName(), metahuman.getGender(), removeNewlinesFromPrompt(metahuman.getDescription()), metahuman.getCreateTime(), metahuman.getUpdateTime());
     }
+
+    private String removeNewlinesFromPrompt(String prompt) {
+        // 将 prompt 中的换行符替换为空格
+        return prompt.replace("\n", " ");
+    }
+
+    @Autowired
+    private LanguageModelService languageModelService;
 
     @PostMapping("/completion")
     public ResponseEntity<String> getCompletion(@RequestBody ChatRequest request) {
@@ -64,7 +70,11 @@ public class OpenAiApiController {
         //将提示作为第一条消息，然后将用户的消息添加到聊天列表中,之后使用这个sessionMessages列表与GPT API进行交互。
         Message initialMessage = new Message("system", prompt);
 
+        //有上下文记忆，但是切换metahuman，就会仍然停留在上一个metahuman
+//        List<Message> sessionMessages = userSessions.getOrDefault(userSessionId, new ArrayList<>());
+        //没有上下文记忆
         List<Message> sessionMessages = new ArrayList<>();
+
         sessionMessages.add(initialMessage);
 
         Message userMessage = new Message("user", userMessageContent);
@@ -72,75 +82,34 @@ public class OpenAiApiController {
 
         userSessions.put(userSessionId, sessionMessages);
 
+        //在运行时，根据配置的 language.model.type 值，动态选择要使用的语言模型服务类。这可以通过条件语句或工厂模式来实现。
+//        String languageModelType = request.getChatModel();
 
-        //可以联系上下文v1.0
-//        String userSessionId = request.getSessionId();
-//        String userMessageContent = request.getUserMessage();
-//
-//        Message userMessage = new Message("user", userMessageContent);
-//        List<Message> sessionMessages = userSessions.getOrDefault(userSessionId, new ArrayList<>());
-//        sessionMessages.add(userMessage);
-//        userSessions.put(userSessionId, sessionMessages);
-
-        //设置接下来和openai api交互 所需要的变量
-        String apiKeyPart1 = "sk-yx3aZJWYqgZ7";
-        String apiKeyPart2 = "MzR4qiz6T3BlbkFJMH";
-        String apiKeyPart3 = "ur8TPYBmvEdSAlammS";
-//        String apiKey = System.getenv("OPENAI_API_KEY");
-        String apiKey = apiKeyPart1 + apiKeyPart2 + apiKeyPart3;
-        String apiUrl = "https://api.openai.com/v1/chat/completions";
-
-        //可以联系上下文的版本 设置payload
-        // Construct the payload using all the messages in the session
-        StringBuilder payloadBuilder = new StringBuilder("{ \"model\": \"gpt-3.5-turbo\", \"messages\": [");
-        for (Message msg : sessionMessages) {
-            payloadBuilder.append("{\"role\": \"").append(msg.getRole()).append("\", \"content\": \"").append(msg.getContent()).append("\"},");
+        String languageModelType = "openai";
+        LanguageModelService selectedLanguageModel;
+        switch (languageModelType) {
+            case "openai":
+                selectedLanguageModel = new OpenAiLanguageModelService(userSessions);
+                break;
+            case "another":
+                selectedLanguageModel = new AnotherLanguageModelService();
+                break;
+            default:
+                // 默认情况下选择 OpenAI 语言模型
+                selectedLanguageModel = new OpenAiLanguageModelService(userSessions);
+                break;
         }
-        payloadBuilder.deleteCharAt(payloadBuilder.length() - 1);  // Remove trailing comma
-        payloadBuilder.append("]}");
-        String payload = payloadBuilder.toString();
 
-        //与openai api交互，发送payload
-        try {
-//            System.out.println(apiKey);
-            URL url = new URL(apiUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-            connection.setDoOutput(true);
+//        String languageModelType = "openai";
+//        if ("openai".equals(languageModelType)) {
+//            languageModelService = new OpenAiLanguageModelService(userSessions);
+//        } else if ("another".equals(languageModelType)) {
+//            languageModelService = new AnotherLanguageModelService();
+//        }
 
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = payload.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
-
-//            int responseCode = connection.getResponseCode();
-//            System.out.println("HTTP Response Code: " + responseCode);
-
-            //接收gpt的回复
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"))) {
-                StringBuilder response = new StringBuilder();
-                String responseLine;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
-                }
-
-                // Extracting assistant's message from the response
-                JSONObject jsonResponse = new JSONObject(response.toString());
-                String assistantMessage = jsonResponse.getJSONArray("choices")
-                        .getJSONObject(0)
-                        .getJSONObject("message")
-                        .getString("content");
-
-                return ResponseEntity.ok(assistantMessage);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).body("Internal Server Error");
-        }
+        // 使用 LanguageModelService 与语言模型进行交互
+        ResponseEntity<String> modelResponse = languageModelService.sendMessage(sessionMessages);
+        // 返回模型的响应
+        return modelResponse;
     }
-
-
 }
